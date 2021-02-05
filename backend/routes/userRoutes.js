4bce86ba221e7f3d783c3f16369bd531e771b877
Sujeet Saha <sujeetsaha@Sujeets-MacBook-Air.local>
2021-02-05 22:12:50 +0530

    Prepare for deployement

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const router = express.Router()
+import { authUser, registerUser, getUserProfile, UpdateUserProfile, getUsers, deleteUser, getUserById, updateUser } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
+
+
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile).put(protect, UpdateUserProfile)
+router.route('/:id').delete(protect, admin, deleteUser).get(protect, admin, getUserById).put(protect, admin, updateUser)
+
+export default router
---
+++ frontend/src/store.js
@@ -0,0 +1,47 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { productListReducers, productDetailsReducers, productDeleteReducer, productCreateReducer, productUpdateReducer, productReviewCreateReducer, productTopRatedReducer } from './reducers/productReducers'
+import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducers, userRegisterReducers, userDetailsReducers, userUpdateProfileReducers, userListReducer, userDeleteReducer, userUpdateReducer } from './reducers/userReducers'
+import { orderCreateReducer, orderDetailsReducer, orderPayReducer, orderListMyReducer, orderListReducer, orderDeliverReducer } from './reducers/orderReducers'
+const reducer = combineReducers({
+    productList: productListReducers,
+    productDetails: productDetailsReducers,
+    productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
+    productTopRated: productTopRatedReducer,
+    cart: cartReducer,
+    userLogin: userLoginReducers,
+    userRegister: userRegisterReducers,
+    userDetails: userDetailsReducers,
+    userUpdateProfile: userUpdateProfileReducers,
+    userList: userListReducer,
+    userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
+    orderPay: orderPayReducer,
+    orderListMy: orderListMyReducer,
+    orderList: orderListReducer,
+    orderDeliver: orderDeliverReducer
+})
+
+const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
+const initialState = {
+    cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
+    userLogin: { userInfo: userInfoFromStorage }
+}
+
+const middleware = [thunk]
+
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
